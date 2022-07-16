0c2da445f72bb36e557a118b9735a8597a43dd35
Kalu <kalu@pop-os.localdomain>
2022-07-16 20:22:41 -0300

    add middleware for error treatment

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { request } from 'http';
+import AppError from '@shared/errors/appError';
 
 const app = express();
 
@@ -8,6 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError)
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
