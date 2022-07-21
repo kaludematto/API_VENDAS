c5ad9dbf19e8d7425615de5a60651177d2b822b0
Kalu <kalu@pop-os.localdomain>
2022-07-21 04:08:15 -0300

    add jwt on sessions service and users

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'eedb141c280f7b0e18c5061be418efdd',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 import UsersControllers from '../controllers/UsersController';
+import isAuthenticated from '../middlewares/isAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersControllers();
 
-usersRouter.get('/', usersController.index);
+usersRouter.get('/', isAuthenticated, usersController.index);
 
 usersRouter.post(
   '/',
